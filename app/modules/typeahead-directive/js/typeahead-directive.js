53418f2288c8943937a629436ba773192e8733f5
Timur Morozov <radist@desant.biz>
2014-06-27 20:17:36 +0300

    typeahead - no need for watch

--- app/modules/typeahead-directive/js/typeahead-directive.js
+++ app/modules/typeahead-directive/js/typeahead-directive.js
@@ -11,22 +11,20 @@ angular.module("typeahead.module", [])
       template: '<input class="typeahead" type="text" placeholder="{{placeholder}}" />',
       link: function(scope, element, attr) {
 
-        function substringMatcher(strs) {
-          return function findMatches(q, cb) {
-            var matches, substringRegex;
-            matches = [];
-            substrRegex = new RegExp(q, 'i');
-            $.each(strs, function(i, str) {
-              if (substrRegex.test(str)) {
-                matches.push({
-                  value: str
-                });
-              }
-            });
+        function findMatches(q, cb) {
+          var matches, substringRegex;
+          matches = [];
+          substrRegex = new RegExp(q, 'i');
+          $.each(scope.suggestions, function(i, str) {
+            if (substrRegex.test(str)) {
+              matches.push({
+                value: str
+              });
+            }
+          });
 
-            cb(matches);
-          };
-        }
+          cb(matches);
+        };
 
         function updateScope(object, suggestion, dataset) {
           scope.$apply(function() {
@@ -43,23 +41,17 @@ angular.module("typeahead.module", [])
         element.bind('typeahead:autocompleted', function(object, suggestion, dataset) {
           updateScope(object, suggestion, dataset);
         });
-        
-        var $typeaheadHolder = element.find(">.typeahead");
-        scope.$watchCollection("suggestions", function(newData) {
-          if (newData && newData.length) {
-            $typeaheadHolder.typeahead('destroy');
-            
-            $typeaheadHolder.typeahead({
-              hint: true,
-              highlight: true,
-              minLength: 1
-            }, {
-              name: 'typeahead',
-              displayKey: 'value',
-              source: substringMatcher(newData)
-            });
-          }
+
+        element.find(">.typeahead").typeahead({
+          hint: true,
+          highlight: true,
+          minLength: 1
+        }, {
+          name: 'typeahead',
+          displayKey: 'value',
+          source: findMatches
         });
+
       } // link -- end
     };
   })
