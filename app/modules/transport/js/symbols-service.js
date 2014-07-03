b0d3afcccd97d6191a7a09e7916850b2d8179ee5
Timur Morozov <radist@desant.biz>
2014-07-03 17:46:08 +0300

    remove extra code in symbols

--- app/modules/transport/js/symbols-service.js
+++ app/modules/transport/js/symbols-service.js
@@ -10,11 +10,30 @@ angular.module("mtvConnection")
     symbols.arr = [];
   }
 
+  function applyUpdates(arr) {
+    var updated = 0;
+    arr.forEach(function(update) {
+      if (update.UpdateType === 0) {
+        if(typeof symbols.keys[update.Symbol.SymbolName] === 'undefined') {
+          symbols.keys[update.Symbol.SymbolName] = update.Symbol.Id;
+          updated += 1;
+        } else {
+          if (symbols.keys[update.Symbol.SymbolName] !== update.Symbol.Id ) {
+            console.log("collision with id", symbols.keys[update.Symbol.SymbolName], update);
+          }
+        }
+      }
+      if (update.UpdateType === 1) {
+        delete symbols.keys[update.Symbol.SymbolName];
+        updated += 1;
+      }
+    });
+    return updated;
+  }
+
   clearSymbols();
 
-  //var symbolUpdates = new Rx.ReplaySubject(1);
-
-  var symbolUpdates = Rx.Observable.create(function (observer) {
+  var symbolUpdatedStreamHandle = Rx.Observable.create(function (observer) {
     var reSubscription = mtvReConnectionOk.stream
       .subscribe(function(x){
         clearSymbols();
@@ -37,42 +56,16 @@ angular.module("mtvConnection")
       console.log("dispose symbolUpdates.reSubscription");
       reSubscription.dispose();
     }
-  });
-
-
-  function applyUpdates(arr) {
-    var updated = 0;
-    arr.forEach(function(update) {
-      if (update.UpdateType === 0) {
-        if(typeof symbols.keys[update.Symbol.SymbolName] === 'undefined') {
-          symbols.keys[update.Symbol.SymbolName] = update.Symbol.Id;
-          updated += 1;
-        } else {
-          if (symbols.keys[update.Symbol.SymbolName] !== update.Symbol.Id ) {
-            console.log("collision with id", symbols.keys[update.Symbol.SymbolName], update);
-          }
-        }
-      }
-      if (update.UpdateType === 1) {
-        delete symbols.keys[update.Symbol.SymbolName];
-        updated += 1;
-      }
-    });
-    return updated;
-  }
-
-  function greaterThenZerro (x) {
+  })
+  .map(applyUpdates) // will return number of applied
+  .filter(function(x) {
     return x > 0;
-  }
-
-  var symbolUpdatedStreamHandle = symbolUpdates
-    .map(applyUpdates) // will return number of applied
-    .filter(greaterThenZerro)
-    //.throttle(2000)
-    .subscribe(function(x) {
-      symbols.arr = _.keys(symbols.keys);
-      console.log("symbols", symbols);
-    });
+  })
+  //.throttle(2000)
+  .subscribe(function(x) {
+    symbols.arr = _.keys(symbols.keys);
+    console.log("symbols", symbols);
+  });
 
   return {
     symbols: symbols,
